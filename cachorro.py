2a25f02feef2860cf4bc227616641c2d6f9126c8
arteweyl <arteweyl@github.com>
2024-01-24 23:45:21 -0300

    criando classe Cachorro

---
+++ cachorro.py
@@ -0,0 +1,8 @@
+class Cachorro:
+    def __init__(self,nome,idade,raca):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+    
+    def  latir(self):
+        print("AuAu!")
