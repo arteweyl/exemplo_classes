d28cd52d29e748d58005f6073b922eb30ee648c2
arteweyl <arteweyl@github.com>
2024-01-24 23:50:51 -0300

    atualizando  classe Cachorro

--- cachorro.py
+++ cachorro.py
@@ -1,8 +1,15 @@
 class Cachorro:
-    def __init__(self,nome,idade,raca):
+    def __init__(self,nome:str,idade:int,raca,albino=False):
         self.nome = nome
         self.idade = idade
         self.raca = raca
-    
+        self.albino = albino
+        if not isinstance(self.nome,str):
+            raise TypeError ("o argumento nome precisa ser uma string")
+        if not isinstance(self.idade,int):
+            raise TypeError ("o argumento nome precisa ser um int")        
     def  latir(self):
         print("AuAu!")
+
+    def morder(self,pessoa):
+        print(f"{self.nome} mordeu {pessoa}")
